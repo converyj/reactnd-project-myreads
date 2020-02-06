a76474cdf37f7ff82e2d3bdc0468709ee37691a5
Jaime Convery <jaimeconvery@gmail.com>
2020-02-06 15:02:37 -0500

    Production Build

---
+++ build/precache-manifest.554a1b9c8ef71d10a45b1377e7994609.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "222729803649421b8a73",
+    "url": "/static/css/main.735e682e.chunk.css"
+  },
+  {
+    "revision": "222729803649421b8a73",
+    "url": "/static/js/main.22272980.chunk.js"
+  },
+  {
+    "revision": "7e9d6572f6bf7cb47197",
+    "url": "/static/js/1.7e9d6572.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1045374d5b976d6a5a811d84349108e9",
+    "url": "/static/media/half-star.1045374d.png"
+  },
+  {
+    "revision": "a56790eae5b8dcda9d8060459db99fe9",
+    "url": "/static/media/empty-star.a56790ea.png"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "7e0b3e37783de307f0a51487c6ba38f5",
+    "url": "/index.html"
+  }
+];
